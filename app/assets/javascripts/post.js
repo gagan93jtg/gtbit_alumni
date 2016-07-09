ecca956b3afda22a69c1fc2cdc0564b1b2a1cbfa
ekas93 <ekaspreet93.singh@gmail.com>
2016-07-09 11:41:47 +0530

    gan_2 UI bootstrapped [home: view post complete, edit_history drawer, icon_border fixed, notifi. in progress]

--- app/assets/javascripts/post.js
+++ app/assets/javascripts/post.js
@@ -1,41 +1,14 @@
-function bind_edit_history_modal_click(id)
-{
-	$('.edit_history').click(function()
-	{
-		$('.loading').css('display','inline-block');
-		$.ajax({
-			url: '/posts/edit_history',
-			type: 'GET',
-			data: {id: id}
-		})
-		.done(function(data)
-		{
-			show_on_modal(data)
-		})
-		.fail(function(error)
-		{
-			console.log("error "+e);
-		})
-		.always(function()
-		{
-			$('.loading').css('display','none');
-		});
+ /*
+       * Open the drawer when the menu ison is clicked.
+       */
+       var menu = document.querySelector('#menu');
+       var main = document.querySelector('.view_query_chain');
+       var drawer = document.querySelector('#drawer');
 
-	});
-
-	$("#edit_history_modal").on('hidden.bs.modal', function ()
-	{
-		$(this).data('bs.modal', null);
-	});
-}
-
-function show_on_modal(data)
-{
-	$('.modal-body').html("<div class='modal_body_question'></div>")
-	for (i = 0 ; i < data.length; i++)
-		$(".modal_body_question").append("\
-			<div class='modal_question'>\
-			<pre class='query_string'> " + data[i].query_string + "</pre>\
-			<span class='tags'> " + data[i].tags + "</span>\
-			</div>");
-}
+       menu.addEventListener('click', function(e) {
+       	drawer.classList.toggle('open');
+       	e.stopPropagation();
+       });
+       main.addEventListener('click', function() {
+       	drawer.classList.remove('open');
+       });
